42b52cdc1569d92a685eb9cb4fcd71c0a6705269
SanskrutiGudhe <sanskrutigudhe2024.comp@mmcoe.edu.in>
2026-02-25 21:49:42 +0530

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,28 +1,28 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function proxy(request: NextRequest) {
-  const { pathname } = request.nextUrl;
+export function proxy(req: NextRequest) {
+  const role = req.cookies.get("role")?.value;
+
+  const { pathname } = req.nextUrl;
 
   if (pathname.startsWith("/dashboard")) {
-    const role = request.cookies.get("role")?.value;
-
     if (!role) {
-      return NextResponse.redirect(new URL("/", request.url));
+      return NextResponse.redirect(new URL("/login", req.url));
     }
 
     if (
       pathname.startsWith("/dashboard/student") &&
       role !== "student"
     ) {
-      return NextResponse.redirect(new URL("/", request.url));
+      return NextResponse.redirect(new URL("/login", req.url));
     }
 
     if (
       pathname.startsWith("/dashboard/printer") &&
       role !== "printer"
     ) {
-      return NextResponse.redirect(new URL("/", request.url));
+      return NextResponse.redirect(new URL("/login", req.url));
     }
   }
 
